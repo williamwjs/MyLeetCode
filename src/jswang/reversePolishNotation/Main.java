4201c6194bdced5ad5bda98604c9d1cc1f9385ee
williamwjs <williamwjs.sjtu@gmail.com>
2014-06-10 02:28:30 +0800

    2 Evaluate Reverse Polish Notation

--- src/jswang/reversePolishNotation/Main.java
+++ src/jswang/reversePolishNotation/Main.java
@@ -5,6 +5,7 @@ package jswang.reversePolishNotation;
  */
 public class Main {
     public static void main(String[] args) {
-
+        Solution rpn = new Solution();
+        System.out.print(rpn.evalRPN(new String[] {"4", "13", "5", "/", "+"}));
     }
 }
--- src/jswang/reversePolishNotation/Solution.java
+++ src/jswang/reversePolishNotation/Solution.java
@@ -2,8 +2,53 @@ package jswang.reversePolishNotation;
 
 /**
  * Created by Jiashuo Wang on 14-6-9.
+ * 1.使用栈来实现
+ * 2.正则表达式要用matches方法
  */
 public class Solution {
+    private class RPNstack {
+        private int length;
+        private int curpos;
+        private String[] stack;
+
+        RPNstack() {
+            length = 20;
+            curpos = 0;
+            stack = new String[length];
+        }
+        void push(String s) {
+            stack[curpos++] = s;
+            if(curpos == length) {
+                length *= 2;
+                String[] tmp = new String[length];
+                for(int i=0; i<curpos; i++) {
+                    tmp[i] = stack[i];
+                }
+                stack = tmp;
+            }
+        }
+        String pop() {
+            return stack[--curpos];
+        }
+        Boolean isEmp() {
+            return curpos == 0;
+        }
+    }
+
     public int evalRPN(String[] tokens) {
+        RPNstack rs = new RPNstack();
+        for(String s : tokens) {
+            if(s.matches("-?[0-9]+"))
+                rs.push(s);
+            else {
+                int x1 = Integer.parseInt(rs.pop());
+                int x2 = Integer.parseInt(rs.pop());
+                if(s.equals("+")) rs.push(Integer.toString(x2+x1));
+                else if(s.equals("-")) rs.push(Integer.toString(x2-x1));
+                else if(s.equals("*")) rs.push(Integer.toString(x2*x1));
+                else if(s.equals("/")) rs.push(Integer.toString(x2/x1));
+            }
+        }
+        return Integer.parseInt(rs.pop());
     }
 }
