36c5fb6a48477ac49122ed43b951a101884ab828
williamwjs <williamwjs.sjtu@gmail.com>
2014-10-04 15:13:59 -0400

    Infinity Knapsack Problem

--- src/jswang/Knapsack_infinity/Solution.java
+++ src/jswang/Knapsack_infinity/Solution.java
@@ -1,14 +1,14 @@
-package jswang.Knapsack1_0;
+package jswang.Knapsack_infinity;
 
 /**
  * Created by JiashuoWang on 10/4/14.
  */
 public class Solution {
-    void Knapsack(int totalV, int[] v, int[] w) { //w是宝石的价值
+    void Knapsack (int totalV, int[] v, int[] w) {
         int[] state = new int[totalV + 1]; //书包从装了0个单位到totalV个单位的总价值
         int totalN = v.length;
         for (int i = 0; i < totalN; i++) {//背包从第0块宝石到遍历到第totalN－1块宝石
-            for (int j = totalV; j >= 0; j--) {
+            for (int j = 0; j <= totalV; j++) {
                 if (j < v[i]) continue;
                 else
                     state[j] = (state[j] > state[j - v[i]] + w[i]) ? state[j] : state[j - v[i]] + w[i];
