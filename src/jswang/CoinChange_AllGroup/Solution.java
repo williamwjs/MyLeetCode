6e920a3179f911a999ba875044f33d49a3ace473
williamwjs <williamwjs.sjtu@gmail.com>
2014-10-11 15:44:49 -0400

    CoinChange_AllGroup

---
+++ src/jswang/CoinChange_AllGroup/Solution.java
@@ -0,0 +1,17 @@
+package jswang.CoinChange_AllGroup;
+
+/**
+ * Created by JiashuoWang on 10/11/14.
+ */
+public class Solution {
+    void coinGroup (int[] s, int n) {
+
+    }
+
+    public static void main (String[] args) {
+        Solution sol = new Solution();
+        int[] s = {1, 7, 10};
+        int n = 14;
+        sol.coinGroup(s, n);
+    }
+}
