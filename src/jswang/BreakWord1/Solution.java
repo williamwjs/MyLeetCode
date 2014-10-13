2b86c411d449053e693a3476ff1d737494dabf5b
williamwjs <williamwjs.sjtu@gmail.com>
2014-10-12 22:33:27 -0400

    wordBreak 1

---
+++ src/jswang/BreakWord1/Solution.java
@@ -0,0 +1,33 @@
+package jswang.BreakWord1;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by JiashuoWang on 10/12/14.
+ */
+public class Solution {
+    public boolean wordBreak(String s, Set<String> dict) {
+        boolean[] state = new boolean[s.length() + 1];
+        state[0] = true;
+
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (state[j] && dict.contains(s.substring(j, i)))
+                    state[i] = true;
+            }
+        }
+
+        return state[s.length()];
+    }
+
+    public static void main (String[] args) {
+        Set<String> dict = new HashSet<String>();
+        dict.add("Leet");
+        dict.add("code");
+        dict.add("a");
+        String s = "Leet";
+        Solution sol = new Solution();
+        System.out.println(sol.wordBreak("aLeetcode", dict));
+    }
+}
