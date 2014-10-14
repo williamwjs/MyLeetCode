d017d37d3719cda14e7c0d27939729df915340f1
williamwjs <williamwjs.sjtu@gmail.com>
2014-10-13 20:28:43 -0400

    BestTimeToBuySellStock 1

--- src/jswang/BestTimeToBuySellStock1/Solution.java
+++ src/jswang/BestTimeToBuySellStock1/Solution.java
@@ -2,10 +2,29 @@ package jswang.BestTimeToBuySellStock1;
 
 /**
  * Created by JiashuoWang on 10/13/14.
- * 首先作差分，然后转化为最大连续子序列和
  */
+//贪婪算法
 public class Solution {
+    int maxProfit (int[] prices) {
+        if (prices.length < 2) return 0;
+
+        int minbuy = prices[0];
+        int profit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            profit = (profit > prices[i] - minbuy) ? profit : prices[i] - minbuy;
+            minbuy = (minbuy < prices[i]) ? minbuy : prices[i];
+        }
+
+        return profit;
+    }
+}
+
+// 首先作差分，然后转化为最大连续子序列和
+class Solution2 {
     int maxProfit (int[] price) {
+        if (price.length < 2) return 0;
+
         for (int i = price.length - 1; i > 0; i--) {
             price[i] -= price[i - 1];
         }
