e3bc6c5c8810a33625476b70094c0169c1b9f15a
williamwjs <williamwjs.sjtu@gmail.com>
2014-11-17 20:38:34 -0500

    Gas Station

---
+++ src/jswang/GasStation/Solution.java
@@ -0,0 +1,30 @@
+package jswang.GasStation;
+
+/**
+ * Created by willwjs on 11/17/14.
+ */
+public class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int total = 0, start = -1;
+
+        for (int i = 0, sum = 0; i < gas.length; ++i) {
+            total += gas[i] - cost[i];
+            sum += gas[i] - cost[i];
+            if (sum < 0) {
+                start = i;
+                sum = 0;
+            }
+        }
+
+        if (total >= 0)
+            return start + 1;
+        else return -1;
+    }
+
+    public static void main (String[] args) {
+        int[] gas = {4};
+        int[] cost = {5};
+        Solution sol = new Solution();
+        System.out.println(sol.canCompleteCircuit(gas, cost));
+    }
+}
