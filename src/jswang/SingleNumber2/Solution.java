b9e78377b01cc35324c2c5db5d2c52d54eecac0d
williamwjs <williamwjs.sjtu@gmail.com>
2014-11-18 15:28:24 -0500

    Single Number 2 with 2 solutions

--- src/jswang/SingleNumber2/Solution.java
+++ src/jswang/SingleNumber2/Solution.java
@@ -22,6 +22,23 @@ public class Solution {
         return tmp;
     }
 
+    public int singleNumber2(int[] A) {
+        int[] bitNum = new int[32];
+
+        for (int i = 0; i < A.length; ++i) {
+            for (int j = 0; j < 32; ++j) {
+                bitNum[j] += (A[i] >> j) & 1;
+                bitNum[j] %= 3;
+            }
+        }
+
+        int tmp = 0;
+        for (int i = 0; i < 32; ++i) {
+            tmp += bitNum[i] << i;
+        }
+        return tmp;
+    }
+
     public static void main (String[] args) {
         Solution sol = new Solution();
         int[] A = {3, 2, 3, 1, 2, 3, 2};
