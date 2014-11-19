891eb3d34f6cd38529ade0c324722f0052184980
williamwjs <williamwjs.sjtu@gmail.com>
2014-11-18 20:06:38 -0500

    Remove Duplicates from Sorted List

--- src/jswang/RemoveDup1/Solution.java
+++ src/jswang/RemoveDup1/Solution.java
@@ -5,12 +5,24 @@ package jswang.RemoveDup1;
  */
 public class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        ListNode tmp = head;
+        if (head == null) return head;
+        ListNode cur = head;
+
+        while (cur.next != null) {
+            if (cur.val == cur.next.val) {
+                cur.next = cur.next.next;
+            }
+            else
+                cur = cur.next;
+        }
+        return head;
+
+        /*ListNode tmp = head;
         for (; tmp != null; tmp = tmp.next) {
             while (tmp.next != null && tmp.next.val == tmp.val) {
                 tmp.next = tmp.next.next;
             }
         }
-        return head;
+        return head;*/
     }
 }
