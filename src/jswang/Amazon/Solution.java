c6374debf12308d377eaf8384b8fa661a8595f32
williamwjs <williamwjs.sjtu@gmail.com>
2015-05-10 21:58:05 +0800

    Amazon Random

--- src/jswang/Amazon/Solution.java
+++ src/jswang/Amazon/Solution.java
@@ -1,11 +1,23 @@
 package jswang.Amazon;
 
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
 /**
  * Created by willwjs on 2/7/15.
  */
 public class Solution {
 
     public static void main (String[] args) {
-
+        Set set = new HashSet();
+        Random random = new Random();
+        while (set.size() != 900) {
+            int num = random.nextInt(1000) + 1;
+            if (!set.contains(num)) {
+                set.add(num);
+            }
+        }
+        System.out.print(set);
     }
 }
