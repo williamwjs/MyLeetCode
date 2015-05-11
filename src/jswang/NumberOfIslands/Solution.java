ddba7501a6a780ce68ace1b4125ee7cd7c0e8c52
williamwjs <williamwjs.sjtu@gmail.com>
2015-05-11 23:23:43 +0800

    Number Of Islands v2

--- src/jswang/NumberOfIslands/Solution.java
+++ src/jswang/NumberOfIslands/Solution.java
@@ -2,42 +2,34 @@ package jswang.NumberOfIslands;
 
 /**
  * Created by willwjs on 5/11/15.
+ * You can't create an array like visited[0][0]
  */
 public class Solution {
     public int numIslands(char[][] grid) {
         if (grid.length == 0 || grid[0].length == 0)
             return 0;
-        boolean[][] visited = new boolean[grid.length][grid[0].length];
         int res = 0;
         for (int i = 0; i < grid.length; ++i) {
             for (int j = 0; j < grid[0].length; ++j) {
-                if (visited[i][j] || grid[i][j] == '0')
+                if (grid[i][j] == '0')
                     continue;
                 res++;
-                findIsland(i, j, grid, visited);
+                findIsland(i, j, grid);
             }
         }
         return res;
     }
 
-    private void findIsland(int i, int j, char[][] grid, boolean[][] visited) {
-        visited[i][j] = true;
-        if (i - 1 >= 0 && !visited[i - 1][j]) {
-            if (grid[i - 1][j] == '1')
-                findIsland(i - 1, j, grid, visited);
-        }
-        if (j - 1 >= 0 && !visited[i][j - 1]) {
-            if (grid[i][j - 1] == '1')
-                findIsland(i, j - 1, grid, visited);
-        }
-        if (j + 1 < grid[0].length && !visited[i][j + 1]) {
-            if (grid[i][j + 1] == '1')
-                findIsland(i, j + 1, grid, visited);
-        }
-        if (i + 1 < grid.length && !visited[i + 1][j]) {
-            if (grid[i + 1][j] == '1')
-                findIsland(i + 1, j, grid, visited);
-        }
+    private void findIsland(int i, int j, char[][] grid) {
+        grid[i][j] = '0';
+        if (i - 1 >= 0 && grid[i - 1][j] == '1')
+                findIsland(i - 1, j, grid);
+        if (j - 1 >= 0 && grid[i][j - 1] == '1')
+                findIsland(i, j - 1, grid);
+        if (j + 1 < grid[0].length && grid[i][j + 1] == '1')
+                findIsland(i, j + 1, grid);
+        if (i + 1 < grid.length && grid[i + 1][j] == '1')
+                findIsland(i + 1, j, grid);
     }
 
     public static void main(String[] args) {
