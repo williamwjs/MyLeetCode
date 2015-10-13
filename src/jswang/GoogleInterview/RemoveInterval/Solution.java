d8e9114c5a789c55c6ee93167beb209d8af06cd2
williamwjs <williamwjs.sjtu@gmail.com>
2015-10-13 11:25:49 -0400

    Remove Interval

---
+++ src/jswang/GoogleInterview/RemoveInterval/Solution.java
@@ -0,0 +1,56 @@
+package jswang.GoogleInterview.RemoveInterval;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by willwjs on 10/13/15.
+ */
+class Interval {
+    int start;
+    int end;
+    Interval() { start = 0; end = 0; }
+    Interval(int s, int e) { start = s; end = e; }
+}
+
+public class Solution {
+    public List<Interval> remove(List<Interval> intervals, Interval removeInterval) {
+        Collections.sort(intervals, (i1, i2) -> i1.start > i2.start ? 1 : -1);
+        List<Interval> res = new LinkedList<>();
+        boolean needDo = true;
+        int start = removeInterval.start, end = removeInterval.end;
+        for (Interval i : intervals) {
+            if (start >= i.end || !needDo) {
+                res.add(i);
+                continue;
+            }
+            if (start > i.start) {
+                res.add(new Interval(i.start, start));
+            }
+            if (end >= i.start && end < i.end) {
+                res.add(new Interval(end, i.end));
+                continue;
+            }
+            if (end < i.start) {
+                res.add(i);
+                needDo = false;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        List<Interval> intervals = new LinkedList<>();
+        intervals.add(new Interval(1, 2));
+        intervals.add(new Interval(4, 5));
+        intervals.add(new Interval(6, 7));
+        intervals.add(new Interval(2, 6));
+        intervals.add(new Interval(1, 5));
+        intervals.add(new Interval(6, 8));
+        intervals.add(new Interval(3, 9));
+        Interval newInterval = new Interval(3, 6);
+        List<Interval> res = new Solution().remove(intervals, newInterval);
+        System.out.println(res);
+    }
+}
